e42ce1a6ece17c1baa8aa0fa6ff4f32cc4f0100b
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-04 11:56:10 +0100

    Initial Commit

---
+++ Locale/LocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleApp.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
