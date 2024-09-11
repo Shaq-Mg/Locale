d7a75fdf43f90510883e07b7294f4be5d8dfde6b
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-11 12:31:57 +0100

    MainMessageVIew UI setup

---
+++ Core/Messenger/MainMessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMessagesView.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 11/09/2024.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMessagesView()
+}
---
+++ ViewControllers/MapViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewController.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 04/09/2024.
+//
+
+import Foundation
