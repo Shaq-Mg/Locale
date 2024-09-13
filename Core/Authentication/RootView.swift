9e32cc4ddc31cdfd99bd9a86e72b10d0714f484d
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-13 23:46:18 +0100

    Check for user & sign out + storeUserInformation function to store data to firebase 🙂

---
+++ Core/Authentication/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 13/09/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
