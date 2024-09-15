ca8d356f45e35241f4cd68c9dc55032825b4102c
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-15 22:53:44 +0100

    ChatView feature added with navigation

---
+++ Core/Messenger/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 15/09/2024.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
