aab3a4e516eed18b9d2865382aa6edb1d87797a0
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-15 19:28:31 +0100

    Show all users for NewMessageView + view model also formed ChatUser data model to identifiable

---
+++ Core/Messenger/CreateNewMessageView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewMessageView.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 15/09/2024.
+//
+
+import SwiftUI
+
+struct CreateNewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewMessageView()
+}
---
+++ Core/Messenger/ViewModel/NewMessageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewMessageViewModel.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 15/09/2024.
+//
+
+import Foundation
