d425121b13cd5a414cf0463091b8bec5e9e06b3f
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-18 12:47:53 +0100

    Fetch chat messages documents and updating ChatViiew UI with ChatMessage data model + FirebaseConstants 😎

---
+++ Extensions/FirebaseConstants.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseConstants.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 18/09/2024.
+//
+
+import Foundation
---
+++ Models/Chat/ChatMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessage.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 18/09/2024.
+//
+
+import Foundation
