201c91fcc465ae4ccc384258559fcb013f60dece
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-01 13:03:18 +0100

    SearchBarView, SearchDestinationView added and presented with binding variable

---
+++ Subviews/Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 01/10/2024.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
