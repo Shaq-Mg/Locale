440533d1d4d8a7c02b17d2ea3403523427381f8c
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-05 16:41:52 +0100

    UPDATE: Adding trip data to destinationDetailView UI

---
+++ Models/Map/Destination.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Locale
+//
+//  Created by Shaquille McGregor on 05/10/2024.
+//
+
+import CoreLocation
+
+struct Destination {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
